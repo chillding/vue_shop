900e7472efe29eaf22828ecd7bd85f71aaa8999e
dingzl <chill_ding@163.com>
2020-07-04 23:32:59 +0800

    åAdd login function

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        home
+        <button type="info" @click="loginOut">退出</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            loginOut(){
+                // 清空token
+                window.sessionStorage.removeItem('token')
+                // 重定向到登录页
+                this.$router.push('/login')
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
